393a8b30a2601153d44ed25c44ea9a7bdf74e0ed
FrolSM <Frolov27101993@yandex.ru>
2026-02-26 19:25:58 +0300

    fix task_04: correct sort_list logic per task requirements

--- task_04.py
+++ task_04.py
@@ -1,8 +1,9 @@
-def  sort_list(lst):
+def sort_list(lst):
     if not lst:
         return lst
-    max_index = lst.index(max(lst))
-    min_index = lst.index(min(lst))
-    lst.append(lst[min_index])
-    lst[max_index], lst[min_index] = lst[min_index], lst[max_index]
-    return lst
+    max_value = max(lst)
+    min_value = min(lst)
+
+    new_lst = [max_value if el == min_value else min_value if el == max_value else el for el in lst]
+    new_lst.append(min_value)
+    return new_lst
